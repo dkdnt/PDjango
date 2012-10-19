7f0327535358967e04cbf430c34e9850edf58083
Juan Roberto Rosales Vargas <juanros13@gmail.com>
2012-10-18 23:38:25 -0500

    Windget error

--- mysite/mysite/apps/home/forms.py
+++ mysite/mysite/apps/home/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class ContactForm(forms.Form):
       Email  = forms.EmailField(widget=forms.TextInput())
       Titulo = forms.CharField(widget=forms.TextInput())
-      Texto  = forms.CharField(widget=forms.TextArea())
+      Texto  = forms.CharField(widget=forms.Textarea)
